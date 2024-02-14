cfd5cbfe0fd3776f56c5885c23e40f6824030a6d
kevjmay <kevinjohnmyburgh@icloud.com>
2024-02-14 17:20:50 +0000

    Displays location/temp data on home page

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ console.log("We will soon find out!")
 const URL_BASE = 'http://api.weatherapi.com/v1/current.json?key='
 const keyA = '88f0546ba04443ef'
 const keyB = 'b01194256241102'
-const locationPlace = 'Brighton'
+let locationPlace = 'Hove'
 
 const URL = `${URL_BASE}${keyA}${keyB}&q=${locationPlace}`
 
 async function getWeather() {
-    const response  = await fetch(URL, {mode: 'cors'});
-    const weatherData = await response.json();
-    console.log(weatherData)
-    console.log(weatherData.current.temp_c)
+
+    const locationSearch = document.querySelector('.locationSearch')
+    const temperature = document.querySelector('.temperature')
+
+    try {
+        const response  = await fetch(URL, {mode: 'cors'});
+        const weatherData = await response.json();
+        console.log(weatherData)
+        locationSearch.textContent = weatherData.location.name
+        temperature.textContent = weatherData.current.temp_c
+    } catch (error) {
+        console.log(error)
+    }
+   
 }
 
 getWeather()
