31d4c88bec2621977397d9009d3adc89ff09480a
kevjmay <kevinjohnmyburgh@icloud.com>
2024-02-14 17:05:10 +0000

    Successful async/await response for data added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("We will soon find out!")
+
+const URL_BASE = 'http://api.weatherapi.com/v1/current.json?key='
+const keyA = '88f0546ba04443ef'
+const keyB = 'b01194256241102'
+const locationPlace = 'Brighton'
+
+const URL = `${URL_BASE}${keyA}${keyB}&q=${locationPlace}`
+
+async function getWeather() {
+    const response  = await fetch(URL, {mode: 'cors'});
+    const weatherData = await response.json();
+    console.log(weatherData)
+    console.log(weatherData.current.temp_c)
+}
+
+getWeather()
