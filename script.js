bee3b2e90d8c7e52ec83b666daa4aafe5834933e
kevjmay <kevinjohnmyburgh@icloud.com>
2024-02-15 20:35:12 +0000

    MVP done, can input location and fetch the temperature

--- script.js
+++ script.js
@@ -5,23 +5,31 @@ const keyA = '88f0546ba04443ef'
 const keyB = 'b01194256241102'
 let locationPlace = 'Hove'
 
-const URL = `${URL_BASE}${keyA}${keyB}&q=${locationPlace}`
+const submitButton = document.querySelector('.submitButton')
+submitButton.addEventListener('click', getWeather)
 
-async function getWeather() {
+const searchInput = document.querySelector('.searchInput')
+
+async function getWeather(e) {
+    e.preventDefault()
 
     const locationSearch = document.querySelector('.locationSearch')
     const temperature = document.querySelector('.temperature')
+
+    locationPlace = searchInput.value
+
+    const URL = `${URL_BASE}${keyA}${keyB}&q=${locationPlace}`
 
     try {
         const response  = await fetch(URL, {mode: 'cors'});
         const weatherData = await response.json();
         console.log(weatherData)
         locationSearch.textContent = weatherData.location.name
-        temperature.textContent = weatherData.current.temp_c
+        temperature.textContent = weatherData.current.temp_c + ' Celsuis'
     } catch (error) {
         console.log(error)
+        locationSearch.textContent = 'Enter a valid city'
+        temperature.textContent = ''
     }
    
 }
-
-getWeather()
